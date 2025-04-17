47425c809c2608a0b998a553982678b0659e7087
himeji. <88136704+h1meji@users.noreply.github.com>
2025-04-17 18:17:41 +0200

    ontick function

--- src/main/java/freck/chunksaddons/ChunksAddons.java
+++ src/main/java/freck/chunksaddons/ChunksAddons.java
@@ -14,15 +14,17 @@ import org.slf4j.LoggerFactory;
 
 public class ChunksAddons implements ModInitializer {
 	public static final String MOD_ID = "chunksaddons";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static MinecraftClient minecraftClient;
 
 
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Initializing ChunksAddons");
+		minecraftClient = MinecraftClient.getInstance();
+	}
+
+	public static void onTick() {
+
 	}
 }
---
+++ src/main/java/freck/chunksaddons/mixin/MinecraftClientMixin.java
@@ -0,0 +1,18 @@
+package freck.chunksaddons.mixin;
+
+import freck.chunksaddons.ChunksAddons;
+import net.minecraft.client.MinecraftClient;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MinecraftClient.class)
+public class MinecraftClientMixin {
+
+    @Inject(method="tick", at=@At("HEAD"))
+    private void onTick(CallbackInfo ci) {
+        ChunksAddons.onTick();
+    }
+
+}
